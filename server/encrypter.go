47ea33fadef49280cbb8e5693698ee80f9490361
noisersup <patryk@kwiatek.xyz>
2021-10-25 20:29:58 +0200

    encrypting bytes

--- server/encrypter.go
+++ server/encrypter.go
@@ -5,6 +5,7 @@ import (
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
+	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -13,6 +14,44 @@ import (
 
 	"github.com/noisersup/encryptedfs-api/logger"
 )
+
+func encryptBytes(input, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	ciphertext := make([]byte, aes.BlockSize+len(input))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return nil, err
+	}
+
+	stream := cipher.NewCFBEncrypter(block, iv)
+	stream.XORKeyStream(ciphertext[aes.BlockSize:], input)
+
+	return ciphertext, nil
+}
+
+func decryptBytes(input, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+
+	if len(input) < aes.BlockSize {
+		return nil, errors.New("input too short")
+	}
+
+	iv := input[:aes.BlockSize]
+	input = input[aes.BlockSize:]
+
+	stream := cipher.NewCFBDecrypter(block, iv)
+
+	stream.XORKeyStream(input, input)
+
+	return input, nil
+}
 
 // Encrypts a file from multipart reader and stores it in provided directory
 func encryptMultipart(r *multipart.Reader, dir string, key []byte) error {
