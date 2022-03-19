f9013fad8c76cf4e9a7b14901d80669fd9722cbe
noisersup <patryk@kwiatek.xyz>
2022-03-19 09:27:36 +0100

    create tables if not exist

--- database/db.go
+++ database/db.go
@@ -35,6 +35,7 @@ func ConnectDB(uri, database string, root string) (*Database, error) {
 	}
 
 	db := Database{pool: pool}
+	db.createIfNotExists()
 
 	err = db.fetchRoot()
 	if err != nil {
@@ -42,6 +43,44 @@ func ConnectDB(uri, database string, root string) (*Database, error) {
 	}
 
 	return &db, nil
+}
+
+func (db *Database) createIfNotExists() {
+	var payloads [4]string
+	payloads[0] = `
+	CREATE TABLE IF NOT EXISTS "file_tree" (
+	  "id" UUID NOT NULL UNIQUE DEFAULT gen_random_uuid(),
+	  "encrypted_name" STRING(255),
+	  "hash" STRING(64),
+	  "duplicate" INT,
+	  "parent_id" UUID,
+	  "is_directory" BOOL,
+	  CONSTRAINT "primary" PRIMARY KEY (id ASC)
+	);
+	`
+
+	payloads[1] = `
+	CREATE TABLE IF NOT EXISTS "file_tree_config" (
+		"root" UUID NOT NULL
+	);
+	`
+
+	payloads[2] = `
+	CREATE UNIQUE INDEX IF NOT EXISTS fileDupliaction ON file_tree (encrypted_name, parent_id);
+	`
+
+	payloads[3] = `
+	CREATE TABLE IF NOT EXISTS "users" (
+		"id" UUID NOT NULL UNIQUE DEFAULT gen_random_uuid(),
+		"username" STRING(16) NOT NULL UNIQUE,
+		"password" STRING(60) NOT NULL,
+		"key"	   STRING(44) NOT NULL UNIQUE,
+		CONSTRAINT "primary" PRIMARY KEY (username)
+	);
+	`
+	for _, payload := range payloads {
+		db.pool.QueryRow(context.Background(), payload)
+	}
 }
 
 // Close database connection
