64d6634ca6cf6d9a60211b97d80c9980abebe418
noisersup <patryk@kwiatek.xyz>
2022-03-19 08:24:14 +0100

    add environment variables for cockroach connection

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"os"
 
 	"github.com/noisersup/encryptedfs-api/database"
 	l "github.com/noisersup/encryptedfs-api/logger"
@@ -12,10 +14,14 @@ func main() {
 	v := flag.Bool("v", false, "verbose output")
 	flag.Parse()
 
+	dbName := getEnv("DB_NAME", "filestorage")
+	user := getEnv("DB_USER", "root")
+	host := getEnv("DB_HOST", "localhost")
+	port := getEnv("DB_PORT", "26257")
+
 	l.Verbose = *v
 
-	dbPayload := "postgresql://root@localhost:26257?sslmode=disable"
-	dbName := "filestorage"
+	dbPayload := fmt.Sprintf("postgresql://%s@%s:%s?sslmode=disable", user, host, port)
 
 	l.LogV("Connecting to database %s with payload: %s", dbName, dbPayload)
 
@@ -28,4 +34,12 @@ func main() {
 	if err = server.InitServer(db); err != nil {
 		l.Fatal(err.Error())
 	}
+}
+
+func getEnv(envName, defValue string) string {
+	env := os.Getenv(envName)
+	if env == "" {
+		return defValue
+	}
+	return env
 }
